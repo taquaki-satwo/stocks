caf27323916e1493b19ca08327a26c1b9eadab35
Takaaki Sato <taquaki-satwo@users.noreply.github.com>
2018-08-21 09:25:45 +0900

    post method

--- routes/stocks.js
+++ routes/stocks.js
@@ -2,11 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 var stockModels = [
-  { id: 1, name: 'iPhoneX 64GB スペースグレイ iOS11' }
+  { id: 1, name: 'iPhone X 64GB スペースグレイ iOS11' },
+  { id: 2, name: 'iPhone X 64GB スペースグレイ iOS11' },
+  { id: 3, name: 'iPhone X 64GB スペースグレイ iOS11' },
+  { id: 4, name: 'iPhone X 64GB スペースグレイ iOS11' },
+  { id: 5, name: 'iPhone X 64GB スペースグレイ iOS11' },
+  { id: 6, name: 'iPhone X 64GB スペースグレイ iOS11' }
 ]
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('stocks', { stockModels: stockModels });
+});
+
+router.post('/', function (req, res, next) {
+  console.log(req.body);
 });
 
 module.exports = router;
